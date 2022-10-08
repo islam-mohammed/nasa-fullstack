459474219619141e67da2c0f69b755225d395d0e
Islam Mohammed <islammohammed1978@gmail.com>
2022-10-08 17:42:38 +0200

    update node.yaml

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,7 +1,5 @@
 const HttpError = require("http-errors");
 module.exports = function (req, res, next) {
-  console.log("///////////////////////");
-  console.log(req.isAuthenticated());
   if (!req.isAuthenticated()) {
     next(new HttpError.Unauthorized("You are not logged in"));
   }
